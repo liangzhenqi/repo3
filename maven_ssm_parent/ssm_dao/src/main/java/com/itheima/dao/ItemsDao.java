e10b0e61598ccf2c61af370dd4390c72279561a3
梁振棋 <869638019@qq.com>
2018-12-23 14:28:40 +0800

    修改ssm_dao

--- maven_ssm_parent/ssm_dao/src/main/java/com/itheima/dao/ItemsDao.java
+++ maven_ssm_parent/ssm_dao/src/main/java/com/itheima/dao/ItemsDao.java
@@ -4,6 +4,11 @@ import com.itheima.domain.Items;
 import org.apache.ibatis.annotations.Select;
 
 public interface ItemsDao {
+    /**
+     * 根据id查询用户信息
+     * @param id
+     * @return
+     */
     @Select("select *from items where id = #{id}")
     Items findByid(Integer id);
 }
